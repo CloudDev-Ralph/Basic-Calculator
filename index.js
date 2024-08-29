ab167a29ba4bee1282ce82f919bad6cb07d82b53
ivanprejoles <ivanpatrickprejoles@gmail.com>
2024-08-29 20:16:10 +0800

    collaboration arithmetic

---
+++ index.js
@@ -0,0 +1,19 @@
+function multiply(num1, num2) {
+    const product = num1 * num2
+    return product
+}
+
+function subtraction(num1, num2) {
+    let difference = num1 - num2
+    return difference
+}
+
+function addition(num1, num2) {
+    let sum = num1 + num2
+    return sum
+}
+
+function division(num1, num2) {
+    let div = num1 / num2
+    return div
+}
